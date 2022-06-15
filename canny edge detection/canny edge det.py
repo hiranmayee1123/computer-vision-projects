db2738739822b2f0b4b718d39b58383798b8b497
M.V.S.Sai Hiranmayee <62197337+hiranmayee1123@users.noreply.github.com>
2022-06-15 09:36:46 +0530

    Add files via upload

---
+++ canny edge detection/canny edge det.py
@@ -0,0 +1,10 @@
+import cv2
+import matplotlib.pyplot as plt
+# Open the image
+img = cv2.imread(r"C:\Users\Mini\Downloads\dancing-spider.jpg" )
+# Apply Canny
+edges = cv2.Canny(img, 100, 200, 3, L2gradient=True)
+plt.figure()
+plt.title('Spider')
+plt.imshow(edges, cmap='gray')
+plt.show()
